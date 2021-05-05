465053ad621ad65600c017f481a27d57107130ac
cpettet <cpettet11@gmail.com>
2021-05-05 09:08:59 -0700

    fixed for heroku deployment

--- config/database.js
+++ config/database.js
@@ -11,4 +11,9 @@ module.exports = {
     dialect: "postgres",
     seederStorage: "sequelize",
   },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
 };
