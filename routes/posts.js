7ca6129abc65c7e6c1c1bbedf6036f62011ce248
Davis <handlerdavis@gmail.com>
2021-05-05 11:08:40 -0600

    added h3

--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,62 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const { csrfProtection, asyncHandler } = require('./utils')
-const { User, Post } = require('../db/models')
-const { requireAuth} = require('../auth')
+const { csrfProtection, asyncHandler } = require("./utils");
+const { User, Post } = require("../db/models");
+const { requireAuth } = require("../auth");
 
-router.get('/', requireAuth, csrfProtection,
-asyncHandler(async(req, res)=>{
-    const currentUserId = req.session.auth.userId
-    const posts = await Post.findAll( {where: {author_id: currentUserId}})
-    res.json({status: 'Ok'})
-}))
+router.get(
+  "/",
+  requireAuth,
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const currentUserId = req.session.auth.userId;
+    const posts = await Post.findAll({ where: { author_id: currentUserId } });
+    res.json({ status: "Ok" });
+  })
+);
 
-router.get('/new', requireAuth, csrfProtection,
-asyncHandler(async(req, res)=>{
-    res.render('create-post', {
-        csrfToken: req.csrfToken()
-    })
-}));
+router.get(
+  "/new",
+  requireAuth,
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    res.render("create-post", {
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
 
-router.post("/", requireAuth, csrfProtection, asyncHandler(async (req, res) => {
-    const { title, postType, isComments, isEmojis, body, recipeId, tagLinksId, isVegetarian, isVegan, isGlutenFree } = req.body;
+router.post(
+  "/",
+  requireAuth,
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const {
+      title,
+      postType,
+      isComments,
+      isEmojis,
+      body,
+      recipeId,
+      tagLinksId,
+      isVegetarian,
+      isVegan,
+      isGlutenFree,
+    } = req.body;
     const author_id = req.session.auth.userId;
-}))
-
-
+    await Post.create({
+      author_id,
+      post_type: postType,
+      title,
+      body,
+      likes: isEmojis,
+      comments: isComments,
+      recipe_id: recipeId,
+      tag_links_id: tagLinksId,
+    });
+    res.redirect("/");
+  })
+);
 
 module.exports = router;
