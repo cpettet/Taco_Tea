295d305b631fe17cf2b4e8bc1f2b9d3c6d8e4c58
Jimmy Ly <lyjimmy19@gmail.com>
2021-07-27 16:46:55 -0400

    finished styling splash page

--- routes/splash.js
+++ routes/splash.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const { Post, sequelize} = require("../db/models");
+const { asyncHandler } = require("./utils");
+
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.render("splash")
-})
+
+router.get("/", asyncHandler(async(req, res, next) => {
+    const posts = await Post.findAll({
+        order: sequelize.random(), limit: 6
+    });
+    const date = posts.forEach((post) =>
+     post.newDate = post.createdAt.toString().split(" ").splice(1,3).join(" "));
+
+    const num = posts.forEach((post, i) =>{post.order = i+1})
+
+    res.render("splash", {posts})
+}))
 
 module.exports = router;
